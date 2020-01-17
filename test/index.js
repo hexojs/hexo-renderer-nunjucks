2d987fa91ccf098a47de05b49bf9c73b3f038611
Thomas Parisot <oncletom@users.noreply.github.com>
2020-01-17 15:58:50 +0100

    Import Hexo tests
    As part of hexojs/hexo#2903

--- test/index.js
+++ test/index.js
@@ -1,49 +1,85 @@
 'use strict';
 
 const should = require('chai').should(); // eslint-disable-line
-const {render} = require('../index.js');
+const {render: r} = require('../index.js');
+const {join} = require('path');
 
-describe('Nunjucks renderer', () => {
+describe('nunjucks', () => {
+  const fixturePath = join(__dirname, 'fixtures', 'hello.njk');
 
-  it('renders from a template', () => {
+  it('render from string', () => {
     const body = [
       'Hello {{ name }}!'
     ].join('\n');
 
-    render({text: body}, {
+    r({text: body}, {
       name: 'world'
     }).should.eql('Hello world!');
   });
 
-  it('"for" tag requires an explicit Array conversion', () => {
+  it('render from path', () => {
+    r({path: fixturePath}, {
+      name: 'world'
+    }).should.eql('Hello world!\n');
+  });
+
+  it('compile from text', () => {
     const body = [
-      '{% for x in arr.toArray() %}',
+      'Hello {{ name }}!'
+    ].join('\n');
+
+    const render = r.compile({
+      text: body
+    });
+
+    render({
+      name: 'world'
+    }).should.eql('Hello world!');
+  });
+
+  it('compile from an .njk file', () => {
+    const render = r.compile({
+      path: fixturePath
+    });
+
+    render({
+      name: 'world'
+    }).should.eql('Hello world!\n');
+  });
+
+  describe('nunjucks filters', () => {
+    const forLoop = [
+      '{% for x in arr | toArray %}',
       '{{ x }}',
       '{% endfor %}'
     ].join('');
 
-    const data = {
-      arr: {
-        toArray: () => {
-          return [1, 2, 3];
+    it('toArray can iterate on Warehouse collections', () => {
+      const data = {
+        arr: {
+          toArray() {
+            return [1, 2, 3];
+          }
         }
-      }
-    };
+      };
 
-    render({text: body}, data).should.eql('123');
-  });
-
-  it('renders a compiled template', () => {
-    const body = [
-      'Hello {{ name }}!'
-    ].join('\n');
-
-    const compiledRender = render.compile({
-      text: body
+      r({text: forLoop}, data).should.eql('123');
     });
 
-    compiledRender({
-      name: 'world'
-    }).should.eql('Hello world!');
+    it('toArray can iterate on plain array', () => {
+      const data = {
+        arr: [1, 2, 3]
+      };
+
+      r({text: forLoop}, data).should.eql('123');
+    });
+
+    it('toArray can iterate on string', () => {
+      const data = {
+        arr: '123'
+      };
+
+      r({text: forLoop}, data).should.eql('123');
+    });
   });
 });
